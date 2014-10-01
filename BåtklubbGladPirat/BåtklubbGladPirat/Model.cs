867b2ec82da278cc9588438ac0f2ad504f8317d8
me222rs <me222rs@student.lnu.se>
2014-10-01 12:22:37 +0200

    Påbörjat uppgiten
    BLAFFF

---
+++ BåtklubbGladPirat/BåtklubbGladPirat/Model.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+using System.Text.RegularExpressions;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+
+namespace BåtklubbGladPirat
+{
+    class Model
+    {
+        private string _path;
+        public string Path      //Validerar sökvägen så att den inte referarar till null, är tom eller bara innehåller whitespaces
+        {
+            get { return _path; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    Console.WriteLine("Strängen är tom eller innehåller mellanslag!");
+                }
+                _path = value;
+            }
+        }
+
+       
+        public Model(string path) 
+        {
+            Path = path;        //initierar fältet _path så att det instansierade objektet innehåller en sökväg
+        }
+
+        public void CreateMember(string namn, int personnummer)
+        {
+            using (StreamWriter writer = new StreamWriter("medlem.txt", true))
+            {
+                string medlemsnummer = createUniqueNumber();
+
+                using (StreamWriter writer2 = new StreamWriter("UniktNummer.txt", true))
+                {
+                    writer2.WriteLine(medlemsnummer);
+                }
+
+                writer.Write("\n" + medlemsnummer + " " +namn +";" + personnummer  + ";");
+
+            }
+        }
+
+
+        
+
+        private static string createUniqueNumber()      //Fungerar bra
+        {
+            bool ifNumberExists = false;
+            int uniqueNumber;
+            string uniqueNumber2 = "";
+            Random random = new Random();
+            uniqueNumber = random.Next(1, 999999);
+            //uniqueNumber = 666;
+            uniqueNumber2 = uniqueNumber.ToString();
+
+            string[] lines = File.ReadAllLines("UniktNummer.txt");
+
+            foreach (string r in lines)
+            {
+                if (r.Equals(uniqueNumber2))
+                {
+                    ifNumberExists = true;
+                    Console.WriteLine("Detta nummer finns redan: " + r);
+                }
+
+            }
+
+            if (ifNumberExists == false)
+            {
+                return uniqueNumber2;
+            }
+            else
+            {
+                createUniqueNumber();
+                return "fel";
+            }
+
+
+
+        }
+
+        public List<string> ViewCompactListMembers()        //Fungerar utmärkt
+        {
+            string[] lines = File.ReadAllLines("medlem.txt");
+            List<string>strArr = new List<string>();
+            foreach (string r in lines)
+            {
+
+                string[] info = r.Split(';');
+                //List<string> arrlist = new List<string>(strArr);
+                strArr.Add(info[0]);
+               
+                //Console.WriteLine("{0}", info[0]);
+            }
+
+            return strArr;
+            
+        }
+
+        public List<string> ViewAllMembers()
+        {
+            string[] lines = File.ReadAllLines("medlem.txt");
+            List<string> strArr = new List<string>();
+            foreach (string r in lines)
+            {
+
+                
+                strArr.Add(r);
+
+                //Console.WriteLine("{0}", info[0]);
+            }
+
+            return strArr;
+
+        }
+    }
+}
---
+++ BåtklubbGladPirat/BåtklubbGladPirat/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BåtklubbGladPirat
+{
+    class Program
+    {
+        private const string memberTextFile = "medlem.txt";
+
+        static void Main(string[] args)
+        {
+            Model model = new Model(memberTextFile); 
+            //List<Recipe> myRecipeList = new List<Recipe>();
+             do
+             {
+                 switch (GetMenuChoice())           //Visar menyn
+                 {
+                     case 0:
+                         return;
+                     case 1:
+                         CreateMember();
+                         break;
+                     case 2:
+                         ViewCompactListMembers();
+                         break;
+                     case 3:
+                         ViewAllMembers();
+                         break;
+
+
+
+                 }
+                 ContinueOnKeyPressed();
+             } while (true);
+        }
+
+        private static void ViewCompactListMembers() 
+        {
+            Model model = new Model(memberTextFile);
+            List<string>members = model.ViewCompactListMembers();
+            foreach (string r in members) 
+            {
+                Console.WriteLine(r);
+            }
+            
+        }
+
+        private static void ViewAllMembers()
+        {
+            Model model = new Model(memberTextFile);
+            List<string> members = model.ViewAllMembers();
+            foreach (string r in members)
+            {
+                Console.WriteLine(r);
+            } 
+        }
+
+        private static void CreateMember() 
+        {
+            Console.Write("Förnamn: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("Personnummer: ");
+            int personNummer = int.Parse(Console.ReadLine());
+
+
+            Model model = new Model(memberTextFile);
+            model.CreateMember(firstName, personNummer);
+        }
+
+        private static void ContinueOnKeyPressed()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Write("\n   Tryck tangent för att fortsätta   ");
+            Console.ResetColor();
+            Console.CursorVisible = false;
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.CursorVisible = true;
+        }
+
+        private static int GetMenuChoice()          //Skriver ut menyn
+        {
+            int choice = 0;
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("=     Den glade piraten     =");
+            Console.ResetColor();
+
+            Console.WriteLine(" - Medlem -----------------------\n");
+            Console.WriteLine(" 0. Avsluta.");
+            Console.WriteLine(" 1. Lägg till medlem.");
+            Console.WriteLine(" 2. Visa kompakt lista.\n");
+            Console.WriteLine(" 3. Visa fullständig lista.\n");
+            Console.WriteLine(" 4. Ta bort medlem.\n");
+            Console.WriteLine(" 5. Redigera medlem.\n");
+            Console.WriteLine(" 6. Visa medlem.\n");
+            Console.WriteLine(" - Båt --------------------\n");
+            Console.WriteLine(" 7. Registrera båt.\n");
+            Console.WriteLine(" 8. Ta bort båt.\n");
+            Console.WriteLine(" 9. Redigera båt.\n");
+
+
+            Console.WriteLine(" ===============================\n");
+            Console.WriteLine(" Ange menyval [0-9]:");
+
+
+            do
+            {
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());         //Läser in ett menyval 0-5 som användaren matar in
+
+                    if (choice > 9 || choice < 0)           //Kastar undantag om det inte är inom intervallet
+                    {
+                        throw new Exception();
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                catch
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("FEL! Det inmatade menyvalet är inte inom intervallet 0-9");
+                    Console.ResetColor();
+                }
+
+            } while (true);
+
+            return choice;   //Returnar menyvalet
+
+        }
+    }
+}
